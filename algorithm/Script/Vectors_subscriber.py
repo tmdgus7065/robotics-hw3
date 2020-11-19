63b645594057a6bb44051f4fe8300979375d245c
stdn.song <333jw@naver.com>
2020-11-19 16:39:47 +0900

    create 3 packages

--- algorithm/Script/Vectors_subscriber.py
+++ algorithm/Script/Vectors_subscriber.py
@@ -2,8 +2,8 @@
 import rospy
 from common_msgs.msg import Vectors
 
-def callback(msg)
-    print "subscribe:", msg.timetable.secs%100, msg.inc.x, msg.inc.y, msg.inc.z
+def callback(msg):
+    print "subscribe:", msg.timetable.secs%100, msg.vect.x, msg.vect.y, msg.vect.z
 
 rospy.init_node('Vectors_subscriber')
 sub = rospy.Subscriber('common_msgs', Vectors, callback)
--- sensor/Script/Vectors_publisher.py
+++ sensor/Script/Vectors_publisher.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import rospy
-from common_msg.msg import Vectors
+from common_msgs.msg import Vectors
 from geometry_msgs.msg import Vector3
 
 rospy.init_node('Vectors_publisher')
@@ -10,8 +10,8 @@ rate = rospy.Rate(1)
 while not rospy.is_shutdown():
     msg.timetable = rospy.get_rostime()
     second = msg.timetable.secs
-    msg.inc = vect(x=second%2, y=second%3, z=second%4)
+    msg.vect = Vector3(x=second%2, y=second%3, z=second%4)
     pub.publish(msg)
-    print "publish", msg.timetable.secs%100, msg.inc.x, msg.inc.y, msg.inc.z
+    print "publish", msg.timetable.secs%100, msg.vect.x, msg.vect.y, msg.vect.z
     rate.sleep()
 
